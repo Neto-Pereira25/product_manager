b74a4b88378af8f85523f871c6684d98c405a68c
Neto-Pereira25 <devneto203@gmail.com>
2025-06-20 22:00:36 -0300

    feat: implement theme mode toggle (dark/light)
    Added support for light and dark themes with a toggle component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider, useTheme } from './theme/ThemeContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+import { Moon, Sun } from 'lucide-react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function MainApp() {
+  const { theme, toggleTheme } = useTheme();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={'min-vh-100 p-4'}>
+      <button onClick={toggleTheme} className='btn btn-outline-secondary mb-3'>
+        <div className='d-flex align-items-center gap-2'>
+          {theme === 'dark' ? (
+            <>
+              <Sun />
+              <span>Light</span>
+
+            </>
+          ) : (
+            <>
+              <Moon />
+              <span>Dark</span>
+            </>
+          )}
+        </div>
+      </button>
+      <h1>🛒 Gerenciador de Produtos</h1>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <ThemeProvider>
+      <MainApp />
+      <ToastContainer />
+    </ThemeProvider>
+  );
+}
+
+export default App;
