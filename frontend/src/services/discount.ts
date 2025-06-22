5852f5431bc882cf9f8294d815bb98e6e567c500
Neto-Pereira25 <devneto203@gmail.com>
2025-06-21 23:32:58 -0300

    feat(api): add removeDiscount function to handle product discount removal

--- frontend/src/services/discount.ts
+++ frontend/src/services/discount.ts
@@ -3,3 +3,7 @@ import api from './api';
 export const getActiveDiscounts = () => {
     return api.get('/api/v1/products/with-discounts/');
 };
+
+export const removeDiscount = (productId: number) => {
+    return api.post(`/api/v1/products/${productId}/discount`);
+};
